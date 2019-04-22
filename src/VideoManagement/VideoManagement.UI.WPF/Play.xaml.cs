dc0b7cab1616c459d5dbac52f28fdddc261e6db1
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-04-22 17:56:07 +0530

    Player with play, pause and stop buttons

--- src/VideoManagement/VideoManagement.UI.WPF/Play.xaml.cs
+++ src/VideoManagement/VideoManagement.UI.WPF/Play.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using VideoManagement.Models;
 
 namespace VideoManagement.UI.WPF
@@ -25,6 +26,42 @@ namespace VideoManagement.UI.WPF
             InitializeComponent();
             Title = video.Name;
             player.Source = new Uri(video.Path, uriKind: UriKind.Absolute);
+            player.Play();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (player.Source != null)
+            {
+                if (player.NaturalDuration.HasTimeSpan)
+                {
+                    lblStatus.Content = String.Format("{0} / {1}", player.Position.ToString(@"mm\:ss"), player.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+                }
+            }
+            else
+            {
+                lblStatus.Content = "No file selected...";
+            }
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
         }
     }
 }
