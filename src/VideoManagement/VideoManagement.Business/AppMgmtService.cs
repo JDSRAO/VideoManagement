b6fe88469201ce898c91e66a0fc0d6c66a600bb4
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-04-24 17:11:08 +0530

    Refactored db creation code

---
+++ src/VideoManagement/VideoManagement.Business/AppMgmtService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VideoManagement.DataAccess.SQLite.Application;
+using VideoManagement.Models.Application;
+
+namespace VideoManagement.Business
+{
+    public class AppMgmtService
+    {
+        private ApplicationDBContext dBContext = new ApplicationDBContext();
+
+        public Guid? GetConnectionStringFor(string path)
+        {
+            var item = dBContext.RecentPaths.Where(x => x.Path.Equals(path)).FirstOrDefault();
+            if(item != null)
+            {
+                return item.ID;
+            }
+            return null;
+        }
+
+        public Guid AddPath(string path, string extension)
+        {
+            var recentPath = new RecentPath()
+            {
+                Path = path
+            };
+            var item = dBContext.RecentPaths.Add(recentPath);
+            dBContext.SaveChanges();
+            return item.Entity.ID;
+        }
+
+        public Guid CreatePathIfNotExists(string path, string extension)
+        {
+            var item = dBContext.RecentPaths.Where(x => x.Path.Equals(path)).FirstOrDefault();
+            if (item == null)
+            {
+                return AddPath(path, extension);
+            }
+            else
+            {
+                return item.ID;
+            }
+        }
+    }
+}
---
+++ src/VideoManagement/VideoManagement.DataAccess/SQLite.Application/Configuration/RecentPathsMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoManagement.Models.Application;
+
+namespace VideoManagement.DataAccess.SQLite.Application.Configuration
+{
+    public class RecentPathsMap : IEntityTypeConfiguration<RecentPath>
+    {
+        public void Configure(EntityTypeBuilder<RecentPath> builder)
+        {
+            builder.HasKey(x => x.ID);
+
+            builder.Property(x => x.Path);
+
+            builder.ToTable("RecentPaths");
+        }
+    }
+}
