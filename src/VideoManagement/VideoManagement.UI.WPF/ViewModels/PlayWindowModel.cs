532d5f2498bd4b8dc90eeb7634a7fd4125ed2fa2
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-04-26 13:49:11 +0530

    MVVM : Added view models to project

---
+++ src/VideoManagement/VideoManagement.UI.WPF/ViewModels/PlayWindowModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoManagement.UI.WPF.ViewModels
+{
+    public class PlayWindowModel
+    {
+    }
+}
