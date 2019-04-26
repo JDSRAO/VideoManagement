14b26142401f8c2ea3310e9146fcd169c716ca8d
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-04-26 15:24:03 +0530

    MVVM : Main window view model setup

--- src/VideoManagement/VideoManagement.UI.WPF/ViewModels/MainWindowModel.cs
+++ src/VideoManagement/VideoManagement.UI.WPF/ViewModels/MainWindowModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VideoManagement.Models;
 
 namespace VideoManagement.UI.WPF.ViewModels
 {
     public class MainWindowModel
     {
+        private string path { get; set; }
+        private string searchQuery { get; set; }
+        private List<Video> videos { get; set; }
+
+        public MainWindowModel()
+        {
+
+        }
     }
 }
