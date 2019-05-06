f5ceed86a24fab79bff91d34f55a176274a2957f
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-05-06 16:12:39 +0530

    Added new table to manage file extensions

---
+++ src/VideoManagement/VideoManagement.DataAccess/SQLite.Application/Configuration/FileExtensionsMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoManagement.Models.Tables;
+
+namespace VideoManagement.DataAccess.SQLite.Application.Configuration
+{
+    public class FileExtensionsMap : IEntityTypeConfiguration<FileExtensions>
+    {
+        public void Configure(EntityTypeBuilder<FileExtensions> builder)
+        {
+            builder.HasKey(x => x.ID);
+
+            builder.Property(x => x.Format);
+
+            builder.ToTable("FileExtensions");
+        }
+    }
+}
---
+++ src/VideoManagement/VideoManagement.Models/Tables/FileExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoManagement.Models.Tables
+{
+    public class FileExtensions : BaseModel
+    {
+        public string Format { get; set; }
+
+
+        public static List<FileExtensions> DefaultExtensions()
+        {
+            var defaultExtensions = new List<string>()
+            {
+                ".avi",".mp4",".mp3"
+            };
+
+            var fileExtensions = new List<FileExtensions>();
+            foreach (var extension in defaultExtensions)
+            {
+                var fileExtension = new FileExtensions()
+                {
+                    ID = Guid.NewGuid(),
+                    Format = extension
+                };
+                fileExtensions.Add(fileExtension);
+            }
+
+            return fileExtensions;
+        }
+    }
+}
