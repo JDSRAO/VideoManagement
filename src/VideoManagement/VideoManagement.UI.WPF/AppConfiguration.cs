4b54386ac70e90b2abe53de961123f96553ba85d
KJDS Srinivasa Rao <kjdssrinivasarao@gmail.com>
2019-06-13 14:40:19 +0530

    Logging setup

---
+++ src/VideoManagement/VideoManagement.UI.WPF/AppConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoManagement.UI.WPF
+{
+    internal static class AppConfiguration
+    {
+        public static string ApplicationLogFileName { get; } = GetValueFromConfig("logging:fileName");
+
+        private static string GetValueFromConfig(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
