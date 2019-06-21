9c2904b8702377d8cc81a1e0fa654f51873eb1a6
KJDS Srinivasa Rao <kjdssrinivasarao@gmail.com>
2019-06-21 13:57:11 +0530

    Copy folder and file path to clipboard

---
+++ src/VideoManagement/VideoManagement.UI.WPF/Helpers/CopyTool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VideoManagement.UI.WPF.Helpers
+{
+    public static class CopyTool
+    {
+        public static void CopyToClipboard(object data)
+        {
+            switch (data)
+            {
+                case string s:
+                    Clipboard.SetText((string)data, TextDataFormat.Text);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
